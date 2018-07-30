ae2562e9d8d2522cc67ab0c8509e5eaa267d54c6
iwag <iwagame@gmail.com>
2018-07-30 01:33:54 -0700

    gen by jhipster

---
+++ src/main/java/io/github/iwag/jblog/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package io.github.iwag.jblog.config.audit;
---
+++ src/main/java/io/github/iwag/jblog/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package io.github.iwag.jblog.repository;
+
+import io.github.iwag.jblog.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
