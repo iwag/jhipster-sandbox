0f7452a946fb90c84c27714626a0b1074ff8f617
iwag <iwagame@gmail.com>
2018-08-11 23:43:20 -0700

    kusa view

--- src/main/webapp/app/entities/kusa-group/kusa-group-detail.tsx
+++ src/main/webapp/app/entities/kusa-group/kusa-group-detail.tsx
@@ -21,6 +21,34 @@ export class KusaGroupDetail extends React.Component<IKusaGroupDetailProps> {
 
   render() {
     const { kusaGroupEntity } = this.props;
+    const WEEK_OF_TABLE = 44;
+    const DAY_OF_WEEK = 7;
+
+    let tables = [];
+    for (let i = 0; i < WEEK_OF_TABLE; i++) {
+      tables[i] = Array(DAY_OF_WEEK).fill({});
+    }
+
+    let today = new Date();
+    today.setDate(today.getDate() + ((6 - today.getDay()) % 7));
+
+    console.log(kusaGroupEntity.actvities);
+    for (let i = WEEK_OF_TABLE - 1; i >= 0; i--) {
+      for (let j = DAY_OF_WEEK - 1; j >= 0; j--) {
+        today.setDate(today.getDate() - 1);
+        let d = today.toISOString().slice(0, 10);
+        let count =
+          kusaGroupEntity.actvities &&
+          kusaGroupEntity.actvities.find((v, i) => v.doneAt && new Date(v.doneAt.toString()).toISOString().slice(0, 10) == d)
+            ? 1
+            : 0;
+        tables[i][j] = {
+          count: count,
+          date: d
+        };
+      }
+    }
+
     return (
       <Row>
         <Col md="8">
@@ -44,6 +72,69 @@ export class KusaGroupDetail extends React.Component<IKusaGroupDetailProps> {
               <Translate contentKey="blogApp.kusaGroup.user">User</Translate>
             </dt>
             <dd>{kusaGroupEntity.user ? kusaGroupEntity.user.login : ''}</dd>
+            <svg width="1200" height="240">
+              {tables.map((row, i) => (
+                <g transform={`translate(${i * 13}, 0)`}>
+                  {row.map((el, j) => (
+                    <rect
+                      width="10"
+                      height="10"
+                      x={i}
+                      y={j * 12 + 30}
+                      fill={el.count > 0 ? '#c6e48b' : '#ebedf0'}
+                      data-count={el.count}
+                      data-date={el.date}
+                    />
+                  ))}
+                </g>
+              ))}
+              <text x="13" y="20">
+                Aug
+              </text>
+              <text x="61" y="20">
+                Sep
+              </text>
+              <text x="109" y="20">
+                Oct
+              </text>
+              <text x="169" y="20">
+                Nov
+              </text>
+              <text x="217" y="20">
+                Dec
+              </text>
+              <text x="277" y="20">
+                Jan
+              </text>
+              <text x="325" y="20">
+                Feb
+              </text>
+              <text x="373" y="20">
+                Mar
+              </text>
+              <text x="421" y="20">
+                Apr
+              </text>
+              <text x="481" y="20">
+                May
+              </text>
+              <text x="529" y="20">
+                Jun
+              </text>
+              <text x="577" y="20">
+                Jul
+              </text>
+            </svg>
+            <dt>
+              {kusaGroupEntity.actvities
+                ? kusaGroupEntity.actvities.map((v, i) => (
+                    <tr>
+                      <td>{v.id}</td>
+                      <td>{v.doneAt}</td>
+                    </tr>
+                  ))
+                : ''}
+            </dt>
           </dl>
           <Button tag={Link} to="/entity/kusa-group" replace color="info">
             <FontAwesomeIcon icon="arrow-left" />{' '}
